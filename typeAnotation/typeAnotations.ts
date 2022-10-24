f685bfc7080e76dbfcf8395bccf9ac1ab4c85415
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-10-24 19:59:09 -0300

    :construction: start

---
+++ typeAnotation/typeAnotations.ts
@@ -0,0 +1,21 @@
+
+//number
+let numero: number = 2;
+//string
+let casa: string = 'mesa'
+ //array
+let itensCarro: string[]= ['bancos', 'volante']
+// boolean
+let verdade: boolean = true;
+// objeto
+let carro: {
+  nome: string;
+  ano: number;
+  preco: number;
+};
+carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 };
+// ==> Functions [Type Annotation]
+function multiplicarNumero(num1: number, num2: number) {
+  return num1 * num2;
+}
+console.log(multiplicarNumero(2, 5));
